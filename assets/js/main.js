c7b0de2aaec520a99fd7f5e7632a6ed578894784
Haneen-Hatem <141676658+Haneen-Hatem@users.noreply.github.com>
2023-09-09 20:35:05 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('.nav-image').click(function(){
+    $('.navbar-nav ').toggleClass('show');
+  })
+  $('.btn').click(function(){
+    $('form').toggleClass('show');
+  })
+})
